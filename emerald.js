05e0d549104b4cc1b255be23648bc8ddfae33851
Nathan Quayle <nathan.quayle@gmail.com>
2018-10-15 21:53:34 +0100

    added alternative way

--- emerald.js
+++ emerald.js
@@ -1,15 +1,33 @@
 const proto = {
     toEq(value) {
-        if (this.obj === value) {
+        if (this.testObj === value) {
             console.log('Passed!')
         } else {
-            console.log(`Expected ${this.obj} to equal ${value}`)
+            console.log(`Expected ${this.testObj} to equal ${value}`)
         }
     }
 }
 
-function emerald(test) {
-    var object = Object.create(proto);
-    object.obj = test;
+function expect(test) {
+    object = Object.create(proto);
+    object.testObj = test;
     return object;
 }
+
+// class emerald {
+//     constructor(testObj) {
+//         this.testObj = testObj;
+//     }
+    
+//     toEq(value) {
+//         if (this.testObj === value) {
+//             console.log('Passed!')
+//         } else {
+//             console.log(`Expected ${this.testObj} to equal ${value}`)
+//         }
+//     }
+// }
+
+// function expect(testObj) {
+//     return new emerald(testObj);
+// }
