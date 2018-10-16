f3dd1c19858389b3a00d13235f5041bbc8f0275a
Nathan Quayle <nathan.quayle@gmail.com>
2018-10-16 12:09:57 +0100

    added more test funcs

--- emerald.js
+++ emerald.js
@@ -1,24 +1,4 @@
-const proto = {
-    toEq(value) {
-        if (this.testObj === value) {
-            console.log('Passed!')
-        } else {
-            console.log(`Expected ${this.testObj} to equal ${value}`)
-        }
-    }
-}
-
-function expect(test) {
-    object = Object.create(proto);
-    object.testObj = test;
-    return object;
-}
-
-// class emerald {
-//     constructor(testObj) {
-//         this.testObj = testObj;
-//     }
-    
+// const proto = {
 //     toEq(value) {
 //         if (this.testObj === value) {
 //             console.log('Passed!')
@@ -28,6 +8,42 @@ function expect(test) {
 //     }
 // }
 
-// function expect(testObj) {
-//     return new emerald(testObj);
+// function expect(test) {
+//     object = Object.create(proto);
+//     object.testObj = test;
+//     return object;
 // }
+
+class emerald {
+    constructor(testObj) {
+        this.testObj = testObj;
+    }
+
+    toEq(value) {
+        if (this.testObj == value) {
+            console.log('Passed!');
+        } else {
+            console.log(`Expected ${this.testObj} to equal ${value}`);
+        }
+    }
+
+    toBe(value) {
+        if (this.testObj === value) {
+            console.log('Passed!');
+        } else { 
+            console.log(`Expected ${this.testObj} to be ${value}`);
+        }
+    }
+
+    toInclude(value) {
+        if(this.testObj.includes(value)) {
+            console.log('Passed!');
+        } else {
+            console.log(`Expected ${this.testObj} to include ${value} `)
+        }
+    }
+}
+
+function expect(testObj) {
+    return new emerald(testObj);
+}
